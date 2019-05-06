55eba6496b1162306c7c73fa648084e1ba6a2d3e
JOÃO VITOR DE OLIVEIRA DA COSTA <16128032@ifc-48901-linux.labinfo.concordia.ifc.edu.br>
2019-05-06 16:03:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/negri/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.negri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void teste(View view){
+        Toast toast = Toast.makeText(this,"Bem Vindo",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
